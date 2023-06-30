9650e22fe7c4edeb808cff4744ca5e224fb7af39
JoshMusira <joshuamusira01@gmail.com>
2023-06-30 11:52:39 +0300

    Fixing bugs

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import './posts.css'
-import Post from '../post/Post'
-import Blog from '../blog/Blog'
-import Blogs from '../blogs/blogs'
+import Blogs from '../blogs/Blogs'
 
 const Posts = () => {
     return (
         <div className='posts'>
-            {/* <Post /> */}
-            {/* <Post /> */}
-            {/* <Post /> */}
             <Blogs />
         </div>
     )
