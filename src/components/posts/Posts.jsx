19db764338c1c7a83da2f65062840a55680c41f6
JoshMusira <joshuamusira01@gmail.com>
2023-06-30 12:35:28 +0300

    Fixing image issue

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './posts.css'
+import Post from '../post/Post'
+// import Blogs from '../blogs/Blogs'
 import Blogs from '../blogs/Blogs'
-
 const Posts = () => {
     return (
         <div className='posts'>
